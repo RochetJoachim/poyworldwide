2f5ce9472054a7fd3ca429ec6861913f02cdb1b6
soufyan <soufyan@koleos.eu>
2026-01-09 12:46:03 +0100

    ***** [ADD] TSK26011957 | POY OPERATIONS - MODIFICATION FACTURE VENTES 2025-12-09 -V1 *****

---
+++ poy_base/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "POY Base Costumizations",
+
+    'summary': "The module includes the costumizations made to feet the costumer's needs",
+
+    'description': """
+Long description of module's purpose
+    """,
+
+    'author': "KOLEOS",
+    'website': "https://www.koleos.eu",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['account_accountant'],
+
+    # always loaded
+    'data': [],
+
+    'assets': {},
+}
+
